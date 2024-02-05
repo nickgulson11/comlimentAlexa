8ffdc4cab898048f45982bad16d0e611d33c6341
nickgulson11 <nickgulson@gmail.com>
2024-02-05 15:41:32 -0600

    Updated prompt-

--- nick-openai.py
+++ nick-openai.py
@@ -6,13 +6,13 @@ apiKey = "XXX"
 client = OpenAI(api_key=apiKey)
 
 def compliment(name):
-    p = "Give a nice compliment to my girlfriend " + name
+    p = "Give a nice and witty two sentence compliment to my girlfriend " + name
     t = 0.7
     response = client.chat.completions.create(
         messages=[{"role": "user", "content": p}],
         model="gpt-3.5-turbo",
         temperature= t,
-        max_tokens = 45
+        max_tokens = 55
     )
     # print(response)
     compliment = response.choices[0].message.content
