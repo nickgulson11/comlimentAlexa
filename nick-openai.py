34b53fdeeda43ca96d9e25d7e149c2aef3e5edba
Gulson <ngulson@deloitte.com>
2024-02-05 15:27:33 -0600

    ChatGPT API Upgrade

--- nick-openai.py
+++ nick-openai.py
@@ -1,15 +1,21 @@
-import openai
+from openai import OpenAI
 import json
 
 # Load your API key from an environment variable or secret management service
-openai.api_key = "XXX"
+apiKey = "XXX"
+client = OpenAI(api_key=apiKey)
 
 def compliment(name):
     p = "Give a nice compliment to my girlfriend " + name
-    response = openai.Completion.create(model="text-davinci-003", prompt=p, temperature=1, max_tokens=90)
-    response = json.loads(str(response))
-    compliment = response['choices'][0]['text']
-    compliment = compliment[2:]
+    t = 0.7
+    response = client.chat.completions.create(
+        messages=[{"role": "user", "content": p}],
+        model="gpt-3.5-turbo",
+        temperature= t,
+        max_tokens = 45
+    )
+    # print(response)
+    compliment = response.choices[0].message.content
     return compliment
 
 print(compliment("emily"))
